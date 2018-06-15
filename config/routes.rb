d3dd4c114bc618077ebfad94f1b9e9ad8ce0459a
Linda <oneill.linda21@gmail.com>
2018-06-15 09:36:01 -0500

    add events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'events/index'
+
+  get 'events/show'
   resources :registered_applications
 
   devise_for :users
@@ -9,6 +12,11 @@ Rails.application.routes.draw do
   get 'welcome/about'
 
   root 'welcome#index'
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
